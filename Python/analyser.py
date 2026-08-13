bdf6993c4a34e17b757e70d811b25ac819e6024f
Anuvansh Choudhary <anuvansh03@gmail.com>
2026-08-13 16:35:52 +0530

    Updated analyser.py

--- Python/analyser.py
+++ Python/analyser.py
@@ -1,7 +1,12 @@
 print("SSH Log Analyzer")
-print("================")
-successful=1
-failed=3
+print("=================")
+successful = 1
+failed = 3
+failed_logins=[
+    "Failed login from ::1",
+    "Failed login from 192.168.1.20",
+    "Failed login from 162.170.2.20"
+]
 print("SSH Security Summary")
 print("--------------------")
 print(f"Successful Logins: {successful}")
@@ -10,3 +15,6 @@ if failed > 0:
     print("Warning: Failed Logins Detected!")
 else:
     print("No Failed Logins Detected.")
+
+for login in failed_logins:
+    print(login)
