8c48fd54a0fad57e1da3f04341e84d4f5554196f
Anuvansh Choudhary <anuvansh03@gmail.com>
2026-08-13 16:45:08 +0530

    Updated Analyser.py.

--- Python/analyser.py
+++ Python/analyser.py
@@ -1,20 +1,8 @@
+import subprocess
 print("SSH Log Analyzer")
 print("=================")
-successful = 1
-failed = 3
-failed_logins=[
-    "Failed login from ::1",
-    "Failed login from 192.168.1.20",
-    "Failed login from 162.170.2.20"
-]
-print("SSH Security Summary")
-print("--------------------")
-print(f"Successful Logins: {successful}")
-print(f"Failed Logins: {failed}")
-if failed > 0:
-    print("Warning: Failed Logins Detected!")
-else:
-    print("No Failed Logins Detected.")
-
-for login in failed_logins:
-    print(login)
+result=subprocess.run(
+    ["sudo","journalctl","-u","ssh","--since","24 hours ago","--no-pager"],
+    capture_output=True, text=True
+)
+print(result.stdout)
