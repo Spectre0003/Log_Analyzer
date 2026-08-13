9114249ba3c1194b781743a820e95baa08ea222d
Anuvansh Choudhary <anuvansh03@gmail.com>
2026-08-13 13:44:53 +0530

    Updated LogScript again and added another screenshot.

--- BashScript/log_script.sh
+++ BashScript/log_script.sh
@@ -4,3 +4,6 @@ successful= $(sudo journalctl -u ssh --since "24 hours ago" | grep "Accepted pas
 failed= $(sudo journalctl -u ssh --since "24 hours ago" | grep "Failed password" | wc -l)
 echo "Successful logins: $successful"
 echo "Failed logins: $failed"
+if [ "$failed" -gt 0 ]; then
+    echo "Warning: Failed Logins Detected!"
+fi
