e628f76eb9786f093aa294204689cb5149a0a188
Anuvansh Choudhary <anuvansh03@gmail.com>
2026-08-13 16:58:06 +0530

    Updated analyser.py

--- Python/analyser.py
+++ Python/analyser.py
@@ -8,9 +8,16 @@ result = subprocess.run(
 )
 logs = result.stdout.splitlines()
 successful = []
+failed=[]
 for line in logs:
     if "Accepted" in line:
         successful.append(line)
+for line in logs:
+    if "Failed" in line:
+        failed.append(line)
 print(f"Successful Logins: {len(successful)}")
 for line in successful:
+    print(line)
+print(f"Failed Logins: {len(failed)}")
+for line in failed:
     print(line)
