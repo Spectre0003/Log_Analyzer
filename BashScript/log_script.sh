26843f56af232fa1f36e4c92a5e719afd583db84
Anuvansh Choudhary <anuvansh03@gmail.com>
2026-08-13 13:58:49 +0530

    Updated LogScript with Screenshots.

--- BashScript/log_script.sh
+++ BashScript/log_script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #Initial script running directly to check logins on ssh
-successful= $(sudo journalctl -u ssh --since "24 hours ago" | grep "Accepted password" | wc -l)
-failed= $(sudo journalctl -u ssh --since "24 hours ago" | grep "Failed password" | wc -l)
+successful= $(sudo journalctl -u ssh --since "24 hours ago" | grep "Accepted" | wc -l)
+failed= $(sudo journalctl -u ssh --since "24 hours ago" | grep "Failed" | wc -l)
 echo "Successful logins: $successful"
 echo "Failed logins: $failed"
 if [ "$failed" -gt 0 ]; then
     echo "Warning: Failed Logins Detected!"
 fi
+echo ""
+echo "Recent Failed Logins:"
+echo "---------------------"
+sudo journalctl -u ssh --since "24 hours ago" --no-pager | grep "Failed"
