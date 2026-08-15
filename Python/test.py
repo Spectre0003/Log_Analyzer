f2c1f186ce7c062b6c0154a078953030d289a13d
Anuvansh Choudhary <anuvansh03@gmail.com>
2026-08-16 01:57:46 +0530

    Added bruteforce detections to test.py

--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,5 @@
 import unittest
-from analyser import parse_events
+from analyser import parse_events,detect_bruteforce
 class TestParser(unittest.TestCase):
     def test_failed_login(self):
         logs = [
@@ -29,5 +29,34 @@ class TestParser(unittest.TestCase):
         ]
         events = parse_events(logs)
         self.assertEqual(len(events), 0)
+    def test_bruteforce_detection(self):
+        logs = [
+            "Aug 14 08:21:33 kali sshd-session[1234]: Failed password for kali from 192.168.1.50 port 50000 ssh2",
+            "Aug 14 08:21:40 kali sshd-session[1235]: Failed password for kali from 192.168.1.50 port 50001 ssh2",
+            "Aug 14 08:21:47 kali sshd-session[1236]: Failed password for kali from 192.168.1.50 port 50002 ssh2"
+        ]
+        events = parse_events(logs)
+        alerts = detect_bruteforce(events)
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0]["severity"], "HIGH")
+        self.assertEqual(alerts[0]["rule"], "SSH_BRUTE_FORCE")
+        self.assertEqual(alerts[0]["source_ip"], "192.168.1.50")
+    def test_no_bruteforce_with_two_failures(self):
+        logs = [
+            "Aug 14 09:21:33 kali sshd-session[1234]: Failed password for kali from 192.168.1.50 port 50000 ssh2",
+            "Aug 14 09:21:40 kali sshd-session[1235]: Failed password for kali from 192.168.1.50 port 50001 ssh2"
+        ]
+        events = parse_events(logs)
+        alerts = detect_bruteforce(events)
+        self.assertEqual(len(alerts), 0)
+    def test_different_ips_do_not_trigger(self):
+        logs = [
+            "Aug 14 10:21:33 kali sshd-session[1234]: Failed password for kali from 192.168.1.10 port 50000 ssh2",
+            "Aug 14 10:21:40 kali sshd-session[1235]: Failed password for kali from 192.168.1.20 port 50001 ssh2",
+            "Aug 14 10:21:47 kali sshd-session[1236]: Failed password for kali from 192.168.1.30 port 50002 ssh2"
+        ]
+        events = parse_events(logs)
+        alerts = detect_bruteforce(events)
+        self.assertEqual(len(alerts), 0)
 if __name__ == "__main__":
     unittest.main()
